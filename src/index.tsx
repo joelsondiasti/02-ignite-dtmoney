d8f03034c3fcb472611f5e1f921622c0eee7fef5
Joelson Dias <joelsondiasti@outlook.com>
2022-06-26 22:37:52 -0300

    End of Module 4 Chapter 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,60 @@
+import { createServer, Model } from "miragejs";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+createServer({
+  models: { transaction: Model },
+ 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de WebSite",
+          amount: 6000,
+          category: "Dev",
+          type: "deposit",
+          createdAt: new Date("2022-06-25 09:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          amount: 1500.3,
+          category: "Moradia",
+          type: "withdraw",
+          createdAt: new Date("2022-06-23 09:00"),
+        },
+        {
+          id: 3,
+          title: "Salário de Professor",
+          amount: 3000,
+          category: "Jobs",
+          type: "deposit",
+          createdAt: new Date("2022-06-10 15:00"),
+        },
+      ],
+    });
+  },
+  
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create("transaction", data);
+    });
+  },
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,8 +7,8 @@ export const GlobalStyle = createGlobalStyle`
     --shape: #ffffff;
 
     --red: #e52e4d;
-    --blue: #5429cc;
     --green: #33cc95;
+    --blue: #5429cc;
 
     --blue-light: #6933ff;
 
@@ -19,6 +19,7 @@ export const GlobalStyle = createGlobalStyle`
 * {
     margin: 0;
     padding: 0 ;
+    outline:0 ;
     box-sizing: border-box ;
 }
 
@@ -56,5 +57,38 @@ h1, h2, h3, h4, h5, h6, strong{
     cursor: not-allowed;
 }
 
+.react-modal-overlay{
+    background-color: rgba(0,0,0,0.5) ;
+    position: fixed ;
+    top: 0 ;
+    bottom: 0 ;
+    right: 0 ;
+    left: 0 ;
 
+    display: flex ;
+    align-items: center ;
+    justify-content: center;
+}
+.react-modal-content{
+    width: 100% ;
+    max-width: 576px;
+
+    background: var(--background) ;
+    padding: 3rem ;
+    position: relative ;
+    border-radius: 0.24rem ;
+}
+
+.react-modal-close{
+    position: absolute;
+    right: 1.5rem ;
+    top: 1.5rem;
+    border: 0 ;
+    background: transparent;
+    transition: filter 0.2s;
+    
+    &:hover{
+        filter: brightness(0.9) ;
+    }
+}
 `;
